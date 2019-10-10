2a3a7617d8e2b22d4b0e7039f85ddef82a84c924
mau211 <mariela.chavez.a@gmail.com>
2019-10-10 15:09:48 -0400

    Updated web config and security config

--- TravelBuddy/src/main/java/com/example/TravelBuddy/config/SecurityConfig.java
+++ TravelBuddy/src/main/java/com/example/TravelBuddy/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -40,21 +39,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
 
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.cors();
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/signup/**", "/login/**").permitAll()
-//                .antMatchers("/user/**", "/profile/**", "/post/**").authenticated()
-//                .antMatchers("/role/**").hasRole("DBA")
-                .antMatchers("/user/**", "/profile/**", "/post/**", "/comment/**","/{username}/**").authenticated()
+                .antMatchers("/signup/**", "/login/**")
+                .permitAll()
+                .antMatchers("/user/**", "/profile/**", "/post/**", "/comment/**")
+                .authenticated()
                 .and()
                 .httpBasic();
-
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
@@ -67,12 +66,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-//    @Override
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception{
-//
-//        User.UserBuilder users = User.withDefaultPasswordEncoder();
-//        auth.inMemoryAuthentication().withUser(users.username("test").password("test").roles("ADMIN"));
-//        auth.inMemoryAuthentication().withUser(users.username("dba").password("dba").roles("DBA"));
-//    }
-
 }
